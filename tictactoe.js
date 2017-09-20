65d75b303fbcebfe53819a9302191d1c22c6de03
ameliarahman <amel.rahman5@gmail.com>
2017-09-20 17:19:09 +0700

    Done

--- tictactoe.js
+++ tictactoe.js
@@ -3,27 +3,25 @@
 function TicTacToe() {
     let xo = ['X', 'O'];
     let arr = [];
+    let temp;
     let x = 0;
     let o = 0;
     for (let r = 0; r < 3; r++) {
         let row = [];
         for (let col = 0; col < 3; col++) {
-            var temp = xo[Math.floor(Math.random() * 2)];
-            console.log(temp);
+            temp = xo[Math.floor(Math.random() * 2)];
+
             if (temp === 'X') {
                 x++;
-                
             } else {
                 o++;
-                
             }
-            console.log('nilai x', x);
-            console.log('nilai o', o);
-            if(x > 5) {
+
+            if (x > 5) {
                 row.push('O');
-            }else if(o > 5){
+            } else if (o > 5) {
                 row.push('X');
-            }else{
+            } else {
                 row.push(temp);
             }
 
